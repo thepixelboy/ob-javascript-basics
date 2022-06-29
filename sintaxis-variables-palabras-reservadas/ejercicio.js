3c38ad4b1dfe9241b242d71059edbcc3fe5285bd
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-06-29 14:21:19 +0000

    feat: add new exercise in the "sintaxis, variables y palabras reservadas" section

---
+++ sintaxis-variables-palabras-reservadas/ejercicio.js
@@ -0,0 +1,18 @@
+// Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+// - Tu nombre (string)
+// - Tu edad (number)
+// - ¿Eres desarrollador? (boolean)
+// - Tu fecha de nacimiento (Date)
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+const lista = [
+  "John Doe",
+  36,
+  true,
+  new Date(1997, 4, 19),
+  libro_favorito = {
+    titulo: "The Lord of the Rings",
+    autor: "J.R.R. Tolkien",
+    fecha: new Date(1954, 6, 1954),
+    url: "https://en.wikipedia.org/wiki/The_Lord_of_the_Rings"
+  }
+]
