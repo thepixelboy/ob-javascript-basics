28cdbe40799362b63825cb4531f29c273277a202
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-07-11 10:33:43 +0000

    feat: add exercise 4 on "trabajando con sets y objetos" section

---
+++ 07-trabajando-con-sets-y-objetos/user-input.js
@@ -0,0 +1,4 @@
+// - Abre una nueva ventana "about:blank" en Google Chrome
+// - Abre la consola de desarrollador de Google Chrome (F12)
+// - Pregunta al usuario cuál es su edad y almacénala en una variable llamada edad
+let edad = prompt("Cuantos años tienes?")
