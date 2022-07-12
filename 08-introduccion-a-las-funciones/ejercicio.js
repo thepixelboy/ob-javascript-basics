a51e925d32b613289261835ffd0b231ed92a565e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-07-12 17:39:01 +0000

    feat: add exercise for "introduction a las funciones" section

---
+++ 08-introduccion-a-las-funciones/ejercicio.js
@@ -0,0 +1,26 @@
+// Crea un archivo JS que contenga las siguientes líneas
+// - Una función sin parámetros que devuelva siempre "true"
+
+function sinParametros() {
+  return true
+}
+
+// - Una función asíncrona que utilice un setTimeout y pase por consola un "Hola soy una promesa" 5 segundos después de haberse ejecutado
+const miPromesa = new Promise((resolve, reject) => {
+  setTimeout(function(){
+    resolve()
+  }, 5000)
+})
+
+miPromesa.then(() => console.log("Hola soy una promesa"))
+// - Una función generadora de índices pares automáticos
+function* generarIndicePar() {
+  let num = 2
+
+  while(true) {
+    num *= 2
+    yield num
+  }
+}
+
+const gen = generarIndicePar()
