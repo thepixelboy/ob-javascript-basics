f5a4c32cebdb8030a82573da1b813812adb5fb92
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-07-17 13:26:53 +0000

    feat: add eslint linter and configure it. modify package.json to create a new script that fixes linting errors. add exercise for "configuracion y  uso de eslint" section

---
+++ 13-configuracion-y-uso-de-eslint/ejercicio.js
@@ -0,0 +1,10 @@
+let nombre = "John"
+let apellido = "Doe"
+let estudiante = `${nombre} ${apellido}`
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus = estudiante.toLowerCase()
+let numeroLetras = estudiante.length
+let primeraLetraNombre = nombre.charAt(0)
+let ultimaLetraApellido = apellido[apellido.length - 1]
+let eliminaEspacios = estudiante.trim()
+let isNombreInEstudiante = estudiante.includes(nombre)
